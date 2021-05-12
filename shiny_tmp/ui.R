14c0f12ac7efaed617285c485a44474279a12e58
Samuel Shamiri <samuelshamiri@yahoo.com>
2021-05-12 10:52:04 +1000

    Update ui.R

--- shiny_tmp/ui.R
+++ shiny_tmp/ui.R
@@ -1,197 +1,146 @@
+
+source('global.R')
+# 
+source('ui_elements/nat_view_ui.R')
+source('ui_elements/reg_view_ui.R')
+source('ui_elements/occ_view_ui.R')
+source('ui_elements/detail_view_ui.R')
+source('ui_elements/method_view_ui.R')
+
 library(shiny)
 library(bs4Dash)
-library(echarts4r)
-library(thematic)
-library(waiter)
-
-thematic_shiny()
-thematic_shiny()
-
-# toast options
-toastOpts <- list(
-  autohide = TRUE,
-  icon = "fas fa-home",
-  close = FALSE,
-  position = "bottomRight"
-)
-
-# echarts4r theme #3d444c
-echarts_dark_theme <- list(
-  options = '{
-    "color":["#6610f2", "#ffc107", "#e83e8c", "#ff851b", "#17a2b8", "#3d9970"], 
-    "backgroundColor": "#343a40", 
-    "textStyle": {
-        color: "#fff"
-    }
-  }',
-  name = "dark_theme"
-)
+library(fresh)
 
 # header
-header <- dashboardHeader(
-  # title = bs4DashBrand(
-  #     title = "bs4Dash",
-  #     color = "primary",
-  #     href = "https://divadnojnarg.github.io/outstanding-shiny-ui/",
-  #     image = "https://adminlte.io/themes/AdminLTE/dist/img/user2-160x160.jpg",
-  #     opacity = 0.8
-  #   ),
-  fixed = TRUE
-)
+HEADER <- bs4DashNavbar(title = dashboardBrand(
+  title = tags$div(img(src="National Skills Commission logo_White.png", width = "180px", height = "80px")),
+  color = "primary",
+  #href = "",
+  #image = "National Skills Commission logo_White.png",
+  opacity = 0.8),
+  tags$strong("NSC Employment Estimates")
+  )
 
-
-
-# navbar
-#navbar = bs4DashNavbar(h2('Nowcasting'))
-
-# sidebar
-# sidebar = dashboardSidebar(
-#   fixed = TRUE, skin = "light", status = "primary", id = "sidebar",
-#   #sidebarUserPanel(name="logo",subtitle = "Test",image = "www/SamuelShamiri.jpg"),
-#   sidebarMenu(
-#     id = "current_tab", flat = FALSE, compact = FALSE, childIndent = TRUE,
-#     bs4SidebarMenuItem("National view", tabName = "national", ionicon(name ="heart")),
-#     menuItem("Regional view", icon = icon("bar-chart-o"), startExpanded = TRUE,
-#              menuSubItem("State", tabName = "state"),
-#              menuSubItem("SA4 level", tabName = "sa4")
-#     ),
-#     menuItem("Occupation view", icon = icon("bar-chart-o"), startExpanded = TRUE,
-#              menuSubItem("ANZSCO4", tabName = "anzsco4"),
-#              menuSubItem("ANZSCO6", tabName = "anzsco6")
-#     ),
-#     menuItem("Detail view", icon = icon("th"), tabName = "detial"),
-#     menuItem("Methodology", icon = icon("th"), tabName = "method")
-#   )
-# )
-sidebar = bs4DashSidebar(
+## sidebar
+SIDEBAR <- bs4DashSidebar(
+  fixed = TRUE,
+  skin = "light",
+  status = "primary",
+  id = "sidebar",
+  #--------------------------------------- menues
   bs4SidebarMenu(
-    #bs4SidebarHeader("Classic Items"),
+    id = "tabname",
     bs4SidebarMenuItem(
       text = "National view",
-      icon = "bars",
-      tabName = "item3"
+      icon = icon("bars"),
+      tabName = "national_tab",
+      badgeLabel = "Soon",
+      badgeColor = "success"
     ),
-    #bs4SidebarHeader("List of items 1"),
     bs4SidebarMenuItem(
       text = "Regional view",
-      icon = "bars",
+      icon = icon("bars"),
       startExpanded = TRUE,
       bs4SidebarMenuSubItem(
         text = "State",
-        tabName = "item1",
-        icon = "circle-thin"
+        tabName = "state_tab",
+        icon = icon("circle-thin")
       ),
       bs4SidebarMenuSubItem(
         text = "SA4 level",
-        tabName = "item2",
-        icon = "circle-thin"
+        tabName = "sa4_tab",
+        icon = icon("circle-thin")
       )
     ),
-    
-    #bs4SidebarHeader("List of items 2"),
     bs4SidebarMenuItem(
       text = "Occupation view",
-      icon = "bars",
+      icon = icon("bars"),
       startExpanded = FALSE,
       #active = FALSE,
       bs4SidebarMenuSubItem(
-        text = "ANZSCO4",
-        tabName = "item4",
-        icon = "circle-thin"
+        text = "ANZSCO 4-digit",
+        tabName = "occ4_tab",
+        icon = icon("circle-thin")
       ),
       bs4SidebarMenuSubItem(
-        text = "ANZSCO6",
-        tabName = "item5",
-        icon = "circle-thin"
+        text = "ANZSCO 6-digit",
+        tabName = "occ6_tab",
+        icon = icon("circle-thin")
       )
+    ),
+    bs4SidebarMenuItem(
+      text = "Detail view",
+      icon = icon("bars"),
+      tabName = "detial_tab"
+    ),
+    bs4SidebarMenuItem(
+      text = "Methodology",
+      icon = icon("bars"),
+      tabName = "method_tab"
     )
   )
 )
 
 # controlbar
-controlbar = dashboardControlbar(id = "controlbar", skin = "light", pinned = TRUE, overlay = FALSE)
+CONTROLBAR = bs4DashControlbar(id = "controlbar", 
+                               skin = "light", 
+                               pinned = FALSE, 
+                               overlay = FALSE,
+                               div(img(src="National Skills Commission logo_RGB.jpg", width = "180px", height = "80px")),
+                               br(),
+                               tags$div('Nowcasting & Economic Modelling', 
+                                        br(),
+                                        ' National Skills Commission' , style = "font-size:15px;padding:5px;"),
+                               
+                               tags$a(href = "www.nationalskillscommission.gov.au", " www.nationalskillscommission.gov.au", style = "font-size:10px;padding:5px;"),
+                               br(), br(),
+                               p(strong("Latest release: "), max(df_list$recent_dates), style = "padding:5px;"),
+                               p(strong("Contact us on: "), 'blablah@skillscommission.gov.au', style = "padding:5px;"),
+                               br(), br(),br(), br(),
+                               p(strong("Disclaimer: "), 'These series remain experimental and are subject to revisions and errors. Caution should be exercised when utilising these employment estimates.', style = "padding:5px;"),
+                               p(strong("Note: "), "A minimum value of 10 has been applied to all estimates.", style = "padding:5px;")
+                               )
 
 # footer
-footer = bs4DashFooter(p('footer'))
+FOOTER = bs4DashFooter(left = 'Copyright © 2021 National Skills Commission All rights reserved.',
+                       right = 'Version 0.1 Beta')
 
-## tabs
-region_tab <- tabItem(
-  tabName = "sa4_plot",
-  fluidRow(
-    selectInput(
-      inputId = "state_filter", 
-      label = "Select state", 
-      choices = unique(df_filter$state_name)
-    ),
-    selectInput(
-      inputId = "sa4_filter", 
-      label = "Select SA4 Region", 
-      choices = NULL
-    ),
-    selectInput(
-      inputId = "anzsco4_filter", 
-      label = "Select ANZSCO4", 
-      choices = NULL
+
+## title
+TITLE = "National Skills Commission"
+
+
+## body
+BODY =  bs4DashBody(
+  
+  use_theme(create_theme(
+    bs4dash_status(
+      primary = "#2E3CB6",
+      secondary = "#2E3CB6"
     )
-  ),
-  fluidRow(
-    box(
-      id = "box_map",
-      title = "Select SA4", 
-      width = 6,
-      status = "danger", 
-      closable = FALSE,
-      maximizable = FALSE, 
-      collapsible = TRUE,
-      p("sa4 map goes here")
-      #plotOutput("sa4_map")
-    ),
-    box(
-      id = "box_sa4_plot",
-      title = "Selected SA4", 
-      width = 6,
-      status = "danger", 
-      closable = FALSE,
-      maximizable = TRUE, 
-      collapsible = TRUE,
-      #p("sa4 plot goes here")
-      echarts4rOutput("sa4_plot")
-    )
+  )),
+  #tags$div(img(src="analytics_32px.png", style="float:left;" ),h1("NSC Employment Estimates")),
+  bs4TabItems(
+    national_view_ui("nat_view"),
+    state_view_ui("state_view"),
+    sa4_view_ui("sa4_view"),
+    occ4_view_ui("occ4_view"),
+    occ6_view_ui("occ6_view"),
+    detail_view_ui("detail_view"),
+    method_view_ui("method_view")
     
   )
 )
 
+# ##
+ui =  bs4DashPage(
+  dark = FALSE,
+  fullscreen = TRUE,
+  scrollToTop = TRUE,
+  header = HEADER,
+  sidebar = SIDEBAR,
+  body = BODY,
+  controlbar = CONTROLBAR,
+  footer = FOOTER,
+  title = TITLE)
 
-
-## body
-body = dashboardBody(
-  e_theme_register(echarts_dark_theme$options, name = echarts_dark_theme$name),
-   tabItems(
-     region_tab
-  #   state_tab,
-  #   sa4_tab,
-  #   anzsco4_tab,
-  #   anzsco6_tab,
-  #   detial_tab,
-  #   method_tab
-   )
-)
-
-## title
-title = "National Skills Commission"
-
-
-##
-ui = dashboardPage( 
-                    # preloader = list(html = tagList(spin_1(), "Loading ..."), color = "#343a40"),
-                    # dark = TRUE,
-                    # help = TRUE,
-                    # fullscreen = TRUE,
-                    # scrollToTop = TRUE,
-                    ####
-                    header,
-                    sidebar, 
-                    controlbar, 
-                    body, 
-                    footer,
-                    title)
